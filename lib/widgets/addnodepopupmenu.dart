aabec7ad6a84571f408982f071ce411b23db656d
Alette <a.s.farzad3@gmail.com>
2025-11-04 15:25:42 +0100

    Popupmenu Setup

---
+++ lib/widgets/addnodepopupmenu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddNotePopupMenu extends StatelessWidget {
+  const AddNotePopupMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Icon(Icons.add),
+    );
+  }
+}
