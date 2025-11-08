323e09d1cd8457fcd5c89171a44c196155c5981b
Alette <a.s.farzad3@gmail.com>
2025-11-08 13:40:28 +0100

    Basic Calander view

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -10,12 +10,9 @@ class CalendarPage extends StatefulWidget {
 
 class _CalendarPageState extends State<CalendarPage> {
 
-  DateTime today = DateTime.now();
-  void _onDaySelected(DateTime day, DateTime focusedDay) {
-    setState(() {
-      today = day;
-    });
-  }
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context){
@@ -23,14 +20,40 @@ class _CalendarPageState extends State<CalendarPage> {
       children: [
         Container(
           child: TableCalendar(
-            locale: 'en_US',
             headerStyle: HeaderStyle(titleCentered: true),
             availableGestures: AvailableGestures.all,
-            focusedDay: today, 
-            selectedDayPredicate: (day)=>isSameDay(day, today),
+            focusedDay: DateTime.now(), 
             firstDay: DateTime.utc(2024,1,1), 
             lastDay: DateTime.utc(2034,1,1),
-            onDaySelected: _onDaySelected,
+            selectedDayPredicate: (day) {
+              // Use `selectedDayPredicate` to determine which day is currently selected.
+              // If this returns true, then `day` will be marked as selected.
+
+              // Using `isSameDay` is recommended to disregard
+              // the time-part of compared DateTime objects.
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                // Call `setState()` when updating the selected day
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                // Call `setState()` when updating calendar format
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              // No need to call `setState()` here
+              _focusedDay = focusedDay;
+            },
             )
         )
       ],      
