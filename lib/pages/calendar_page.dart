c5d4fcd507fd7d93b66a038a2f7b88de0a29fb78
Alette <a.s.farzad3@gmail.com>
2025-11-08 13:17:51 +0100

    Adding calendar view

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Text('Awesome Calendar Page');
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+        Container(
+          child: TableCalendar(
+            locale: 'en_US',
+            headerStyle: HeaderStyle(titleCentered: true),
+            availableGestures: AvailableGestures.all,
+            focusedDay: today, 
+            selectedDayPredicate: (day)=>isSameDay(day, today),
+            firstDay: DateTime.utc(2024,1,1), 
+            lastDay: DateTime.utc(2034,1,1),
+            onDaySelected: _onDaySelected,
+            )
+        )
+      ],      
+    );
   }
 }
