eb15f116077072974f117ce9edfd2594fee443aa
Niels862 <vschagenniels@gmail.com>
2025-11-12 17:12:21 +0100

    replaced md viewer with quill editor

--- lib/pages/note_viewer_page.dart
+++ lib/pages/note_viewer_page.dart
@@ -1,28 +1,70 @@
-import 'package:flutter/material.dart';
-import 'package:free_note/models/note.dart';
-import 'package:free_note/widgets/markdown_viewer.dart';
-import 'package:go_router/go_router.dart';
+import 'dart:convert';
 
-class NoteViewerPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:free_note/event_logger.dart';
+import 'package:free_note/models/note.dart';
+import 'package:go_router/go_router.dart';
+import 'package:flutter_quill/flutter_quill.dart';
+
+class NoteViewerPage extends StatefulWidget {
   final Note note;
   const NoteViewerPage({super.key, required this.note});
 
   @override
+  State<NoteViewerPage> createState() => _NoteViewerPageState();
+}
+
+class _NoteViewerPageState extends State<NoteViewerPage> {
+  final QuillController _controller = QuillController.basic();
+  bool editing = true;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    try {
+      _controller.document = Document.fromJson(jsonDecode(widget.note.content));
+    } on FormatException {
+      logger.e('Unconverted note: "${widget.note.title}" (#${widget.note.id})');
+    }
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text(note.title, style: Theme.of(context).textTheme.titleLarge),
+        title: Text(widget.note.title, style: Theme.of(context).textTheme.titleLarge),
         actions: [
           IconButton(
             onPressed: () {
-              context.push('/note/${note.id}/edit', extra: note);
+              setState(() {
+                editing = !editing;
+              });
             },
-            icon: Icon(Icons.edit),
+            icon: Icon(
+              editing 
+              ? Icons.remove_red_eye 
+              : Icons.edit
+            ),
           ),
         ],
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: MarkdownViewer(data: note.content),
+      body: Column(
+        children: [
+          QuillSimpleToolbar(
+            controller: _controller,
+            config: const QuillSimpleToolbarConfig(),
+          ),
+          Expanded(
+            child: QuillEditor.basic(
+              controller: _controller,
+              config: const QuillEditorConfig(),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
