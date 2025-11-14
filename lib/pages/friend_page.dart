40e68bf28df01a4e1dd7199652f50da96eeea993
Alette <a.s.farzad3@gmail.com>
2025-11-14 15:06:31 +0100

    Friends page

--- lib/pages/friend_page.dart
+++ lib/pages/friend_page.dart
@@ -3,6 +3,7 @@ import 'package:free_note/event_logger.dart';
 import 'package:free_note/providers/auth_provider.dart';
 import 'package:free_note/services/auth_service.dart';
 import 'package:go_router/go_router.dart';
+import 'package:popover/popover.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -11,31 +12,33 @@ class FriendPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<String> friends = ["Niels", "Niels2"];
 
-    User? user = AuthService.instance.user;
+  List<String> friends = ["Niels", "Niels2"];
 
-    if (user == null) {
-      logger.e('Invalid state: no logged in user');
-      context.go('/login');
-    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friends', style: Theme.of(context).textTheme.titleLarge),
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.person_add)),
+        ],
+      ),
+      body: Container(
+        child: ListView.builder(
+          itemCount: friends.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(child: Text(friends[index][0]),),
+              title: Text(friends[index]),
+              subtitle: Text(''),
+              trailing: Text(''),
+              onLongPress: () {
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Container(
-          child: ListView.builder(
-            itemCount: friends.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(child: Text(friends[index][0]),),
-                title: Text(friends[index]),
-                subtitle: Text(''),
-                trailing: Text(''),
-              );
-            })
-        )
-      );
-    }
+              }
+            );
+          })
+      )
+    );
   }
 }
