17af2af3d3dee382aec0d3245a8ea719c1946b10
Alette <a.s.farzad3@gmail.com>
2025-11-14 15:47:04 +0100

    This works

--- lib/pages/friend_page.dart
+++ lib/pages/friend_page.dart
@@ -20,7 +20,15 @@ class FriendPage extends StatelessWidget {
         title: Text('Friends', style: Theme.of(context).textTheme.titleLarge),
         actions: [
           IconButton(
-            onPressed: () {}, 
+            onPressed: () {
+              showSearch(context: context, delegate: FriendSearch());
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+
+            }, 
             icon: Icon(Icons.person_add)),
         ],
       ),
@@ -39,6 +47,83 @@ class FriendPage extends StatelessWidget {
             );
           })
       )
+    );
+  }
+}
+
+
+class FriendSearch extends SearchDelegate {
+  List<String> searchTerms = [
+    'Apple',
+    'Banana',
+    'Pear',
+  ]; //TODO: Replace this list by what you pull from database!
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    //TODO: Fix Search Function / make it more efficient
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(
+            result,
+          ), //returns the name as fruit as index tile on the found search answers
+          //TODO: Potentially change what it shows, maybe show the context of the note too?
+          //TODO: And then instead of "Text" it should probably be a textbutton that shows part of the thing and that as function opens the editor on that note
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var fruit in searchTerms) {
+      if (fruit.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(fruit);
+      }
+    }
+    return ListView.builder(
+      itemCount: matchQuery.length,
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(
+            result,
+          ), //returns the name as fruit as index tile on the found search answers
+          //TODO: Potentially change what it shows, maybe show the context of the note too?
+        );
+      },
     );
   }
 }
