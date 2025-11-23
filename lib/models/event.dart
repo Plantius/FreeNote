35f84de8a189f40ec4a0413ab09c58275e3f71f1
Niels862 <vschagenniels@gmail.com>
2025-11-23 19:23:10 +0100

    event model

---
+++ lib/models/event.dart
@@ -0,0 +1,26 @@
+import 'package:calendar_view/calendar_view.dart';
+
+class Event {
+  final int id;
+  final String title;
+  final DateTime start;
+  final DateTime end;
+
+  Event({
+    required this.id,
+    required this.title,
+    required this.start,
+    required this.end,
+  });
+
+  CalendarEventData<Event> toCalendarEvent() {
+    return CalendarEventData<Event>(
+      date: start,
+      startTime: start,
+      endTime: end,
+      title: title,
+      description: '<empty description>',
+      event: this,
+    );
+  }
+}
